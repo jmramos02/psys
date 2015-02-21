f539b245eab98a5fb013892c833e2b3d5562ebba
jmramos02 <ramosjosemari@gmail.com>
2015-02-21 22:45:01 +0800

    Changed connection string

--- connString.php
+++ connString.php
@@ -1,8 +1,8 @@
 <?php
-	$link = mysqli_connect("0.0.0.0", "admin", "Jeremy03") or die("Error " . mysqli_error($link));
+	$link = mysqli_connect("0.0.0.0", "root", "unix") or die("Error " . mysqli_error($link));
 
 	function conString($query){
-		$link = mysqli_connect("0.0.0.0", "admin", "Jeremy03") or die("Error " . mysqli_error($link));
+		$link = mysqli_connect("0.0.0.0", "root", "unix") or die("Error " . mysqli_error($link));
 		$result = $link->query($query);
 		return $result;
 	}
