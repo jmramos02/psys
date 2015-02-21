aabcd33d62df19e018e69967a8b1dec6c2bbff43
jmramos02 <ramosjosemari@gmail.com>
2015-02-21 23:05:12 +0800

    Inaus ung semi colon -_-

--- checkPW.php
+++ checkPW.php
@@ -2,7 +2,7 @@
     SESSION_START();
     //error_reporting(0);
     include 'connString.php';
-	$username = mysqli_escape_string($link ,$_POST['empNum']);
+    $username = mysqli_escape_string($link ,$_POST['empNum']);
     $pw = mysqli_escape_string($link, $_POST['pw']);
     $query = "SELECT * from employeemasterfile.emplogin where employeeNum = '$username' and pass = '$pw';" or die("Error. " . mysqli_error($link));
     $result = conString($query);
@@ -11,7 +11,7 @@
     if ($rows == 1){
         $query = "SELECT PositionCode, lastname, firstname, middlename FROM employeemasterfiles.employee WHERE empnum = '$username';" or die("Error. " . mysql_error($link));
         $result = conString($query); 
-        $row = mysqli_fetch_array($result)
+        $row = mysqli_fetch_array($result);
         $_SESSION['posCode'] = $row[0];
         $_SESSION['lastname'] = $row[1];
         $_SESSION['firstname'] = $row[2];
@@ -22,14 +22,11 @@
         echo $row[2];
         echo $row[3];
         
-        //header("Location:../psys/");
-    }
-
-    echo 'abc';
-    /*else{?>
-        <?php echo "<script>
+        header("Location:../psys/");
+    }else{
+        echo "<script>
                 alert('Invalid Employee Number or Password');
                 window.location.href='login.php';
             </script>";
-    }*/
+    }
 ?>
